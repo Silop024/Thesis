eb888234da92047f90b9c8326a60b5bcee89c07d
Silop024 <jackmdwebb@hotmail.com>
2023-06-13 19:37:09 +0200

    Updated classifications

--- Method_OpenCV/humoments_svm_classifier.py
+++ Method_OpenCV/humoments_svm_classifier.py
@@ -1,6 +1,7 @@
 import os
 import joblib
 import cv2
+import configparser
 import numpy as np
 import preprocessing
 from processing import read_image_and_process
@@ -9,9 +10,11 @@ from prediction import ShapePrediction
 class ShapeClassifier:
     def __init__(self, model_path: str):
         self.clf = joblib.load(model_path)
+        self.predictions: list[ShapePrediction]
 
-    def classifyShapes(self, image_path: str, debug: bool = False) -> list[ShapePrediction]:
+    def classifyShapes(self, image_path: str, debug: bool = False):
         processed_image = read_image_and_process(image_path, debug)
+        self.predictions = []
         
         X = []
         for huMoment in processed_image['humoments']:
@@ -20,57 +23,41 @@ class ShapeClassifier:
         X = np.array(X)
         classifications = self.clf.predict(X)
         
-        predictions = []
+        # Only useful for the one class classifier that acts as a rejecter.
         for i, centroid in enumerate(processed_image['centroids']):
             if classifications[i] == -1:
-                predictions.append(ShapePrediction(i, centroid, False, "", 0))
+                self.predictions.append(
+                    ShapePrediction(
+                        index = i, 
+                        position = centroid, 
+                        is_keyword = False, 
+                        keyword = '', 
+                        id = 0, 
+                        image_path = image_path
+                    )
+                )
             else:
-                predictions.append(ShapePrediction(i, centroid, True, classifications[i], 0))
+                self.predictions.append(
+                    ShapePrediction(
+                        index = i, 
+                        position = centroid, 
+                        is_keyword = True, 
+                        keyword = str(classifications[i]),
+                        id = -1,
+                        image_path = image_path
+                    )
+                )
+                
         
-        for prediction in predictions:
-            print(prediction)
-            
-        #self.showPredictions(image_path, predictions)
+    def showPredictions(self, image_path: str):
+        if len(self.predictions) < 1:
+            return
         
-        return predictions
-        
-    def showPredictions(self, image_path: str, predictions: list[ShapePrediction]):
         image = preprocessing.read_image_and_scale(image_path)
         
-        for prediction in predictions:
-            cv2.putText(image, f'{prediction.index}: {prediction.keyword}', prediction.position, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 0, 255), thickness=1)
+        for prediction in self.predictions:
+            cv2.putText(image, prediction.__repr__(), prediction.position, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 0, 255), thickness=1)
         
         cv2.imshow('Predictions', image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    # Directory where the trained model will be stored
-    model_dir = 'C:\\Users\\Jack\\Desktop\\Thesis\\Trained_Models\\'
-
-    # Directory where the training images are stored
-    image_dir = 'C:\\Users\\Jack\\Desktop\\Thesis\\Test_Images\\'
-    
-    rejecter = ShapeClassifier(model_path=f'{model_dir}humoments_one_svm_model.pkl')
-    
-    classifier = ShapeClassifier(model_path=f'{model_dir}humoments_multi_svm_model.pkl')
-    # Get a list of all the image files in the directory
-    image_files = os.listdir(image_dir)
-    
-    # Process each image file
-    for image_file in image_files:
-        image_path = f'{image_dir}{image_file}'
-        predictions_rejecter = rejecter.classifyShapes(image_path)
-        predictions_classifier = classifier.classifyShapes(image_path)
-
-        to_remove = []
-        for i in range(len(predictions_rejecter)):
-            if predictions_rejecter[i].is_keyword == False:
-                to_remove.append(predictions_classifier[i])
-        
-        for i in to_remove:
-            predictions_classifier.remove(i)
-            
-        classifier.showPredictions(f'{image_dir}{image_file}', predictions_classifier)
-        
--- Method_OpenCV/prediction.py
+++ Method_OpenCV/prediction.py
@@ -1,30 +1,59 @@
+import cv2
 from dataclasses import dataclass
+import preprocessing
 
 @dataclass
 class ShapePrediction:
     """Class for keeping track of the classification of a shape
     
     Fields:
-        index -- The position of the shape in relation to other shapes, 0 is the first shape.
+        index (int) The position of the shape in relation to other shapes, 0 is the first shape.
 
-        position -- The absolute position of the shape in the image (x, y).
+        position (tuple) The absolute position of the shape in the image (x, y).
     
-        is_keyword -- A flag to know if the shape is a restricted keyword
+        is_keyword (bool) A flag to know if the shape is a restricted keyword
     
-        keyword -- The keyword that the shape represents, if any.
+        keyword (str) The keyword that the shape represents, if any.
     
-        id -- The id of the shape, given that is does not represent a keyword.
+        id (int) The id of the shape, given that is does not represent a keyword.
+        
+        image_path (str) The path to the image where the shape was classified. For debugging.
     """
     index: int
     position: tuple
     is_keyword: bool
     keyword: str
     id: int
+    image_path: str
+    
+    def show(self):
+        image = preprocessing.read_image_and_scale(self.image_path)
+        cv2.putText(image, self.__repr__(), self.position, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 0, 255), thickness=1)
+        cv2.imshow('Predictions', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
 
-    def __str__(self):
+    def __str__(self) -> str:
         string = f'index: {self.index}\nposition: {self.position}\n'
-        if (self.is_keyword):
+        if self.is_keyword:
             string += f'keyword: {self.keyword}\n'
         else:
             string += f'id: {self.id}\n'
         return string
+    
+    
+    def __repr__(self) -> str:
+        string = f'{self.index}: '
+        if self.is_keyword:
+            string += self.keyword
+        else:
+            string += str(self.id)
+        return string
+    
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ShapePrediction):
+            return False
+        
+        return self.index == other.index and self.image_path == other.image_path
