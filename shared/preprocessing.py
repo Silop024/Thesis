60799e5c6523f2c66e3960f6f2c4b298363509ae
Silop024 <jackmdwebb@hotmail.com>
2023-07-13 14:46:43 +0200

    Changed preprocessing, added unit tests

--- shared/preprocessing.py
+++ shared/preprocessing.py
@@ -1,54 +1,36 @@
 import cv2
 import numpy as np
-
-def preprocess_image(image: np.ndarray) -> np.ndarray:
-    # Convert image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Blur image
-    blur = cv2.GaussianBlur(gray, (5,5), 0) 
-    
-    return blur
-
-def read_image_and_preprocess(image_path: str) -> np.ndarray:
-    #image = read_image_and_scale(image_path)
-    image = read_image(image_path)
-    
-    preprocessed_image = preprocess_image(image)
-    
-    return preprocessed_image
+import skimage.filters
 
 def scale_image(image: np.ndarray) -> np.ndarray:
-    # Desired maximum size
-    max_size = 480
-
-    # Calculate the ratio of the new size to the old size
-    old_size = image.shape[:2]
-    ratio = float(max_size) / max(old_size)
-
-    # Calculate the new dimensions
-    new_dim = (int(image.shape[1] * ratio), int(image.shape[0] * ratio))
+    width_scale, height_scale = detect_image_scale(image)
     
-    resized_image = cv2.resize(image, new_dim)
-    
-    delta_w = max_size - new_dim[1]
-    delta_h = max_size - new_dim[0]
-    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
-    left, right = delta_w // 2, delta_w - (delta_w // 2)
+    new_dim = (image.shape[0] / width_scale, image.shape[1] / height_scale)
 
-    color = [255, 255, 255]
-    resized_image_with_border = cv2.copyMakeBorder(resized_image, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                                   value=color)
-    
-    #cv2.imshow('ROI', resized_image_with_border)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    resized_image = cv2.resize(src=image, dsize=new_dim)
         
-    return resized_image_with_border
+    return resized_image
+
+
+def enhance_image(image: np.ndarray) -> np.ndarray:
+    enhanced_image = skimage.filters.unsharp_mask(image)
+    
+    return enhanced_image
+
+
+def morph_image(image: np.ndarray) -> np.ndarray:
+    morphed_image = cv2.morphologyEx(src=image, op=cv2.MORPH_CLOSE)
+    
+    return morphed_image
+
+
+def segment_image(image: np.ndarray) -> np.ndarray:
+    _, segmented_image = cv2.threshold(image, 0, 255)
+    
+    return segmented_image
 
 
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-
 def detect_image_scale(image: np.ndarray) -> tuple[int, int]:
     marker_params =  cv2.aruco.DetectorParameters_create()
     marker_corners, _, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters=marker_params)
@@ -64,11 +46,6 @@ def detect_image_scale(image: np.ndarray) -> tuple[int, int]:
     height_scale = marker_height_px / 6
 
     return width_scale, height_scale
-
-
-def read_image_and_scale(image_path: str) -> np.ndarray:
-    image = read_image(image_path)
-    return scale_image(image)
 
 
 def read_image(image_path: str) -> np.ndarray:
