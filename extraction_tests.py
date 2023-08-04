22a857e906cb7e71bf27d4374b36f8ea0d67da5a
Silop024 <jackmdwebb@hotmail.com>
2023-08-05 01:02:23 +0200

    Much cleaner code and visualization

--- extraction_tests.py
+++ extraction_tests.py
@@ -1,18 +1,15 @@
 import shared.feature_extraction as extraction
 import shared.preprocessing
+from shared.features import FeatureType
+import shared.processing as processing
 
 import configparser
 import os
-import cv2
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
-from sklearn.manifold import TSNE
 
-import sklearn.preprocessing
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 from typing import Tuple
 
@@ -21,59 +18,42 @@ def test_hu_moments(images: Tuple[str, np.ndarray]):
     X = []  # features
     Y = []  # labels
     
-    for shape_name, preprocessed_image in images: 
-        #rois = extraction.get_regions_of_interest(preprocessed_image)
-        rois = extraction.get_contours(preprocessed_image)
+    for image_tuple in images: 
+        x, y = extraction.extract_all_features(image_tuple, FeatureType.HuMoments)
         
-        for roi in rois:
-            features = shared.feature_extraction.get_hu_moments(roi)
-            
-            X.append(features)
-            Y.append(shape_name)
-            
-    scaler = sklearn.preprocessing.StandardScaler()
-    X = np.array(X)
-    X = scaler.fit_transform(X)
+        X.extend(x)
+        Y.extend(y)
+         
+    X, Y = processing.scale_data(X, Y)
     
-    #pca = PCA(3)
-    #X = pca.fit_transform(X)
-        
-    encoder = sklearn.preprocessing.LabelEncoder()
-    Y_le = encoder.fit_transform(Y)
-    Y = encoder.inverse_transform(Y_le)
+    n_components = 7
+    pca = PCA(n_components=n_components)
+    X = pca.fit_transform(X)
     
-    show_features(X, Y)
+    show_features(X, Y, pca, n_components)
             
 
 def test_zernike_moments(images: Tuple[str, np.ndarray]):
     X = []  # features
     Y = []  # labels
     
-    for shape_name, preprocessed_image in images:
-        contours = extraction.get_contours(preprocessed_image)
+    for image_tuple in images:
+        x, y = extraction.extract_all_features(image_tuple, FeatureType.ZernikeMoments)
         
-        for contour in contours:
-            features = shared.feature_extraction.get_zernike_moments(contour, preprocessed_image)
-            
-            X.append(features)
-            Y.append(shape_name)
-            
-    scaler = sklearn.preprocessing.StandardScaler()
-    X = np.array(X)
-    X = scaler.fit_transform(X)
+        X.extend(x)
+        Y.extend(y)
     
-    #pca = PCA(7)
-    #X = pca.fit_transform(X)
-        
-    encoder = sklearn.preprocessing.LabelEncoder()
-    Y_le = encoder.fit_transform(Y)
-    Y = encoder.inverse_transform(Y_le)
+    X, Y = processing.scale_data(X, Y)
     
-    show_features(X, Y)
+    n_components = 7
+    pca = PCA(n_components=n_components)
+    X = pca.fit_transform(X)
+    
+    show_features(X, Y, pca, n_components)
             
 # TODO: Fix SIFT with Bag-Of-Words or K-Means clustering or other things.
-def test_sift(images: Tuple[str, np.ndarray]):
-    descriptors = []
+def test_sift(images: list[Tuple[str, np.ndarray]]):
+    """descriptors = []
     for _, preprocessed_image in images:
         rois = extraction.get_regions_of_interest(preprocessed_image)
         
@@ -83,8 +63,8 @@ def test_sift(images: Tuple[str, np.ndarray]):
             if features is not None:
                 descriptors.extend(features)
 
-    clusters = 100
-    kmeans = KMeans(n_clusters=clusters)
+    clusters = 10
+    kmeans = KMeans(n_clusters=clusters, n_init='auto')
     kmeans.fit(descriptors)
     codebook = kmeans.cluster_centers_
 
@@ -115,88 +95,44 @@ def test_sift(images: Tuple[str, np.ndarray]):
             histogram = np.array(histogram)
             
             X.append(histogram)
-            Y.append(shape_name)
+            Y.append(shape_name)"""
+            
+    codebook, descriptors = extraction.create_codebook(images)
+            
+    X = []
+    Y = []
+    for image_tuple in images:
+        x, y = extraction.extract_all_sift(image_tuple, codebook, descriptors)
+        
+        X.extend(x)
+        Y.extend(y)
 
-    X = np.array(X)
-    Y = np.array(Y)
+    X, Y = processing.scale_data(X, Y)
     
-    print(X.shape)
-    print(Y.shape)
-    """
-    unique_labels = np.unique(Y)
-    for label in unique_labels:
-        mean_histogram = X[Y == label].mean(axis=0)
-        plt.bar(range(len(mean_histogram)), mean_histogram, alpha=0.5, label=f'Shape {label}')
-    plt.title('Mean Histograms by Label')
-    plt.xlabel('Bin')
-    plt.ylabel('Count')
-    plt.legend()
-    plt.show()
-    
-    for label in unique_labels:
-        distances = pdist(X[Y == label], metric='euclidean')
-        plt.hist(distances, bins=50, alpha=0.5, label=f'Shape {label}')
-    plt.title('Histograms of Histogram Distances by Label')
-    plt.xlabel('Distance')
-    plt.ylabel('Count')
-    plt.legend()
-    plt.show()
-    
-    tsne = TSNE(n_components=2)
-    X_tsne = tsne.fit_transform(X)
-
-    for label in unique_labels:
-        plt.scatter(X_tsne[Y == label, 0], X_tsne[Y == label, 1], label=f'Shape {label}')
-    plt.title('t-SNE Projection of Histograms by Label')
-    plt.xlabel('Component 1')
-    plt.ylabel('Component 2')
-    plt.legend()
-    #plt.show()
-
-    pca = PCA(n_components=2)
-    X_pca = pca.fit_transform(X)
-    
-    for label in unique_labels:
-        plt.scatter(X_pca[Y == label, 0], X_pca[Y == label, 1], label=f'Shape {label}')
-    plt.title('PCA Projection of Histograms by Label')
-    plt.xlabel('Component 1')
-    plt.ylabel('Component 2')
-    plt.legend()
-    plt.show()"""
-    
-    #tsne = TSNE(n_components=3)
-    #X = tsne.fit_transform(X)
-    pca = PCA(n_components=3)
+    n_components = 7
+    pca = PCA(n_components=n_components)
     X = pca.fit_transform(X)
     
-    show_features(X, Y)
+    show_features(X, Y, pca, n_components)
     
 
 def test_hog(images: Tuple[str, np.ndarray]):
     X = []  # features
     Y = []  # labels
     
-    for shape_name, preprocessed_image in images:
-        rois = extraction.get_regions_of_interest(preprocessed_image)
+    for image_tuple in images:
+        x, y = extraction.extract_all_features(image_tuple, FeatureType.HOG)
         
-        for roi in rois:
-            features = shared.feature_extraction.get_hog(roi)
+        X.extend(x)
+        Y.extend(y)
             
-            X.append(features)
-            Y.append(shape_name)
-
-    scaler = sklearn.preprocessing.StandardScaler()
-    X = np.array(X)
-    X = scaler.fit_transform(X)
+    X, Y = processing.scale_data(X, Y)
     
-    pca = PCA(n_components=20)
+    n_components = 7
+    pca = PCA(n_components=n_components)
     X = pca.fit_transform(X)
-        
-    encoder = sklearn.preprocessing.LabelEncoder()
-    Y_le = encoder.fit_transform(Y)
-    Y = encoder.inverse_transform(Y_le)
     
-    show_features(X, Y)
+    show_features(X, Y, pca, n_components)
 
 def test_all(image_paths):
     images = []
@@ -210,38 +146,40 @@ def test_all(image_paths):
         images.append((shape_name, preprocessed_image))
     
     
-    #test_hu_moments(images)
-    #test_zernike_moments(images)
-    #test_hog(images)
-    test_sift(images)
+    test_hu_moments(images)
+    test_zernike_moments(images)
+    test_hog(images)
+    #test_sift(images)
 
 
-def show_features(X, Y):
-    # Plot the scaled features
-    num_features = X.shape[1]
-    num_labels = len(np.unique(Y))
-    num_rows = int(np.ceil(np.sqrt(num_features)))
-    num_cols = int(np.ceil(num_features / num_rows))
+def show_features(X_pca, Y, pca: PCA, n_components: int):
+    total_var = pca.explained_variance_ratio_.sum() * 100
+    
+    exp_var_cumul = np.cumsum(pca.explained_variance_ratio_)
 
-    fig, axes = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(15, 12))
-    axes_flat = axes.ravel()
-
-    ax: np.ndarray
-    for i in range(num_features):
-        ax = axes_flat[i]
-        for label in np.unique(Y):
-            mask = Y == label
-            ax.scatter(np.arange(X.shape[0])[mask], X[:, i][mask], label=f'Shape {label}')
-
-        ax.set_title(f'Feature {i}')
-
-    for i in range(num_features, num_rows * num_cols):
-        axes_flat[i].axis('off')
-
-    handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', ncol=len(labels))
-    plt.tight_layout()
-    plt.show()
+    fig = px.area(
+        x=range(1, exp_var_cumul.shape[0] + 1),
+        y=exp_var_cumul,
+        labels={"x": "# Components", "y": "Explained Variance"}
+    )
+    fig.show()
+    
+    """fig = px.scatter_matrix(
+        X_pca,
+        labels=Y,
+        dimensions=range(n_components),
+        color=Y
+    )
+    fig.update_traces(diagonal_visible=False)
+    fig.show()"""
+    
+    fig = px.scatter_3d(
+        X_pca, x=0, y=1, z=2, 
+        labels=Y,
+        color=Y,
+        title=f'Total Explained Variance: {total_var:.2f}%',
+    )
+    fig.show()
 
 
 
