d8ab1f4986d2d0d304ca3969d6bc5ab917ae444a
Silop024 <jackmdwebb@hotmail.com>
2023-08-09 16:56:10 +0200

    Changed debugging, moved sorting

--- src/classification.py
+++ src/classification.py
@@ -1,5 +1,5 @@
 # My own files
-from feature_extraction import extract_all_features, get_contours
+from feature_extraction import extract_all_features, get_contours, show_features
 from features import FeatureType
 import processing
 import preprocessing
@@ -12,74 +12,41 @@ from typing import List, Tuple
 # Installed with pip
 import cv2
 import joblib
-import matplotlib
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.base import BaseEstimator
 
-matplotlib.rcParams['figure.figsize'] = (20.0, 10.0)
-matplotlib.rcParams['image.cmap'] = 'gray'
 
-
-def classify(image_path: str, clf: BaseEstimator, pca: PCA):
+def classify(image_path: str, clf: BaseEstimator, rej: BaseEstimator, pca: PCA):
     image = preprocessing.read_image(image_path)
     
     preprocessed_image = preprocessing.preprocess_image(image)
     
-    X, _ = extract_all_features(("ERROR", preprocessed_image), FeatureType.HOG)
+    X, Y = extract_all_features(("N/A", preprocessed_image), FeatureType.HOG)
     
     # Process data
     X = processing.scale_data(X)
     X = processing.use_pca(X, pca)
     
     # Classify, returns a list of labels
-    #predictions = clf.predict(X)
+    predictions = clf.predict(X)
+    rejections = rej.predict(X)
     
-    contours = get_contours(preprocessed_image)        
+    show_features(X, predictions, pca)
+    
+    """contours = get_contours(preprocessed_image)        
     
     backtorgb = cv2.cvtColor(preprocessed_image, cv2.COLOR_GRAY2RGB)
     
-    contours_sorted = sort_shapes(contours[:-1])
-    
-    for i, c in enumerate(contours_sorted):
-        x, y, w, h = c
+    for i, c in enumerate(contours):
+        cv2.drawContours(backtorgb, [c], -1, (0, 255, 0), 2)
         
-        cv2.rectangle(backtorgb, (x, y), (x + w, y + h), (255,0,0), 2)
-        cv2.putText(backtorgb, str(i), (x + int(w / 2), y + int(h / 2)),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        print(f"Prediction: {predictions[i]}")
+        print(f"Rejection: {'Defined' if rejections[i] == 1 else 'Undefined'}")
         
-    cv2.imshow("", backtorgb)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    
-
-
-def sort_shapes(contours: List) -> List[Tuple]:
-    bboxes = [cv2.boundingRect(i) for i in contours]
-    
-    c = np.array(bboxes)
-    max_height = np.max(c[::, 3])
-    # Sort the contours by y-value
-    by_y = sorted(bboxes, key=lambda x: x[1])  # y values
-
-    line_y = by_y[0][1]       # first y
-    line = 1
-    by_line = []
-
-    # Assign a line number to each contour
-    for x, y, w, h in by_y:
-        if y > line_y + max_height:
-            line_y = y
-            line += 1
-        
-        by_line.append((line, x, y, w, h))
-
-    # This will now sort automatically by line then by x
-    contours_sorted = [(x, y, w, h) for line, x, y, w, h in sorted(by_line)]
-    
-    return contours_sorted
-	
+        cv2.imshow("", backtorgb)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()"""
 
 
 if __name__ == '__main__':
@@ -93,6 +60,8 @@ if __name__ == '__main__':
     clf_path = os.path.join(model_dir, "clf.joblib")
     
     clf = joblib.load(os.path.join(model_dir, "clf.joblib"))
+    rej = joblib.load(os.path.join(model_dir, "rej.joblib"))
     pca = joblib.load(os.path.join(model_dir, "pca.joblib"))
     
-    classify(image_path, clf, pca)
+    classify(image_path, clf, rej, pca)
+    
